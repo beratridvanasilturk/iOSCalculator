d20e356bf9ab5bf74fc6f398fca2abee2d99413a
beratridvanasilturk <br.asltrk@gmail.com>
2023-06-18 23:44:28 +0300

    Created Buttons

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Berat Rıdvan Asiltürk on 18.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func ceButton(_ sender: UIButton) {
+    }
+    
+}
+
