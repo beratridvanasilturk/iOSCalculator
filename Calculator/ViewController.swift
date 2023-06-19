669574e9471f67f3c26962710f5ee692f85413d1
beratridvanasilturk <br.asltrk@gmail.com>
2023-06-19 18:38:28 +0300

    Added Equal Type Func. and Numbers UI Updates

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,8 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    typealias AdditionHandler = () -> Int
-    
     @IBOutlet var resultLabel: UILabel! {
         didSet {
             resultLabel.text = ("\(resultNumber)")
@@ -29,9 +27,46 @@ class ViewController: UIViewController {
     var groupNumbers: Array<Int> = []
     var firstClick = 0
 
+    var additionButtonTapped : Bool = false
+    var minusButtonTapped: Bool = false
+    var percentButtonTapped: Bool = false
+    var multiplyButtonTapped: Bool = false
+    var divideButtonTapped: Bool = false
+    
+    
     func equalType() {
-        additionButton(UIButton())
+        
+        if additionButtonTapped == true {
+            additionButton(UIButton())
+        }
+        
+        if minusButtonTapped == true {
+            minusButton(UIButton())
+        }
+        
+        if percentButtonTapped == true {
+            percentButton(UIButton())
+        }
+        
+        if multiplyButtonTapped == true {
+            multiplyButton(UIButton())
+        }
+        
+        if divideButtonTapped == true {
+            divideButton(UIButton())
+        } else {
+            print("Button algilanamadi")
+        }
     }
+    
+    @IBAction func equalButton(_ sender: UIButton) {
+        
+        equalType()
+        
+        resultLabel.text = "\(resultNumber)"
+        print("equal buttondaki result number:", resultNumber)
+    }
+    
     
     @IBAction func ceButton(_ sender: UIButton) {
         
@@ -45,18 +80,51 @@ class ViewController: UIViewController {
     }
     
     @IBAction func percentButton(_ sender: UIButton) {
+        
+        percentButtonTapped = true
+        
     }
     
     @IBAction func multiplyButton(_ sender: UIButton) {
+        
+        multiplyButtonTapped = true
+        
     }
     
     @IBAction func divideButton(_ sender: UIButton) {
+        
+        divideButtonTapped = true
+        
     }
     
     @IBAction func minusButton(_ sender: UIButton) {
+        
+        minusButtonTapped = true
+        
+        var minusResult = 0
+        
+        if firstClick == 1 {
+            
+            resultNumber = firstNumber
+            minusResult = resultNumber
+            print("FIRST CLICK'TE minus icindeki result number:", resultNumber, "first click:", firstClick)
+            
+        } else {
+            
+            resultNumber -= secondNumber
+            minusResult = resultNumber
+            print("SECOND CLICK'TE addition icindeki result number:", resultNumber, "else icindeki first click:", firstClick)
+        }
+        
+        resultNumber = minusResult
+        
+            print("addition icindeki result number:", resultNumber)
+        
     }
     
     @IBAction func additionButton(_ sender: UIButton) {
+        
+        additionButtonTapped = true
         
         var additionResult = 0
         
@@ -79,13 +147,6 @@ class ViewController: UIViewController {
         
     }
     
-    @IBAction func equalButton(_ sender: UIButton) {
-        
-        equalType()
-        
-        resultLabel.text = "\(resultNumber)"
-        print("equal buttondaki result number:", resultNumber)
-    }
     
     @IBAction func zeroButton(_ sender: UIButton) {
         
@@ -109,14 +170,14 @@ class ViewController: UIViewController {
             
             let firstOneNumber = numbers[1]
             firstNumber = firstOneNumber
-            print("first click:",firstClick)
+            resultLabel.text = "\(firstOneNumber)"
             firstClick = 1
-            print("first click:", firstClick)
             
         } else {
             
             let secondOneNumber = numbers[1]
             secondNumber = secondOneNumber
+            resultLabel.text = "\(secondOneNumber)"
             firstClick += 1
         }
     }
@@ -127,56 +188,135 @@ class ViewController: UIViewController {
             
             let firstTwoNumber = numbers[2]
             firstNumber = firstTwoNumber
+            resultLabel.text = "\(firstTwoNumber)"
             firstClick = 1
             
         } else {
             
             let secondTwoNumber = numbers[2]
             secondNumber = secondTwoNumber
+            resultLabel.text = "\(secondTwoNumber)"
             firstClick += 1
         }
     }
     
     @IBAction func threeButton(_ sender: UIButton) {
-        let threeNumber = numbers[3]
-        groupNumbers.append(threeNumber)
-        print("\(threeNumber)")
+        if firstClick == 0 {
+            
+            let firstThreeNumber = numbers[3]
+            firstNumber = firstThreeNumber
+            resultLabel.text = "\(firstThreeNumber)"
+            firstClick = 1
+            
+        } else {
+            
+            let secondThreeNumber = numbers[3]
+            secondNumber = secondThreeNumber
+            resultLabel.text = "\(secondThreeNumber)"
+            firstClick += 1
+        }
     }
     
     @IBAction func fourButton(_ sender: UIButton) {
-        let fourNumber = numbers[4]
-        groupNumbers.append(fourNumber)
-        print("\(fourNumber)")
+        if firstClick == 0 {
+            
+            let firstFourNumber = numbers[4]
+            firstNumber = firstFourNumber
+            resultLabel.text = "\(firstFourNumber)"
+            firstClick = 1
+            
+        } else {
+            
+            let secondFourNumber = numbers[4]
+            secondNumber = secondFourNumber
+            resultLabel.text = "\(secondFourNumber)"
+            firstClick += 1
+        }
     }
     
     @IBAction func fiveButton(_ sender: UIButton) {
-        let fiveNumber = numbers[5]
-        groupNumbers.append(fiveNumber)
-        print("\(fiveNumber)")
+        if firstClick == 0 {
+            
+            let firstFiveNumber = numbers[5]
+            firstNumber = firstFiveNumber
+            resultLabel.text = "\(firstFiveNumber)"
+            firstClick = 1
+            
+        } else {
+            
+            let secondFiveNumber = numbers[5]
+            secondNumber = secondFiveNumber
+            resultLabel.text = "\(secondFiveNumber)"
+            firstClick += 1
+        }
     }
     
     @IBAction func sixButton(_ sender: UIButton) {
-        let sixNumber = numbers[6]
-        groupNumbers.append(sixNumber)
-        print("\(sixNumber)")
+        if firstClick == 0 {
+            
+            let firstSixNumber = numbers[6]
+            firstNumber = firstSixNumber
+            resultLabel.text = "\(firstSixNumber)"
+            firstClick = 1
+            
+        } else {
+            
+            let secondSixNumber = numbers[6]
+            secondNumber = secondSixNumber
+            resultLabel.text = "\(secondSixNumber)"
+            firstClick += 1
+        }
     }
     
     @IBAction func sevenButton(_ sender: UIButton) {
-        let sevenNumber = numbers[7]
-        groupNumbers.append(sevenNumber)
-        print("\(sevenNumber)")
+        if firstClick == 0 {
+            
+            let firstSevenNumber = numbers[7]
+            firstNumber = firstSevenNumber
+            resultLabel.text = "\(firstSevenNumber)"
+            firstClick = 1
+            
+        } else {
+            
+            let secondSevenNumber = numbers[7]
+            secondNumber = secondSevenNumber
+            resultLabel.text = "\(secondSevenNumber)"
+            firstClick += 1
+        }
     }
     
     @IBAction func eightButton(_ sender: UIButton) {
-        let eightNumber = numbers[8]
-        groupNumbers.append(eightNumber)
-        print("\(eightNumber)")
+        if firstClick == 0 {
+            
+            let firstEightNumber = numbers[8]
+            firstNumber = firstEightNumber
+            resultLabel.text = "\(firstEightNumber)"
+            firstClick = 1
+            
+        } else {
+            
+            let secondEightNumber = numbers[8]
+            secondNumber = secondEightNumber
+            resultLabel.text = "\(secondEightNumber)"
+            firstClick += 1
+        }
     }
     
     @IBAction func nineButton(_ sender: UIButton) {
-        let nineNumber = numbers[9]
-        groupNumbers.append(nineNumber)
-        print("\(nineNumber)")
+        if firstClick == 0 {
+            
+            let firstNineNumber = numbers[9]
+            firstNumber = firstNineNumber
+            resultLabel.text = "\(firstNineNumber)"
+            firstClick = 1
+            
+        } else {
+            
+            let secondNineNumber = numbers[9]
+            secondNumber = secondNineNumber
+            resultLabel.text = "\(secondNineNumber)"
+            firstClick += 1
+        }
     }
     
     @IBAction func dotButton(_ sender: UIButton) {
