1fa782b83c13cb64b2205234925139dae9c46943
beratridvanasilturk <br.asltrk@gmail.com>
2023-06-22 02:40:25 +0300

    Dot Logic Issue Need Fix

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -5,12 +5,13 @@
 //  Created by Berat Rıdvan Asiltürk on 18.06.2023.
 //
 
-// TODO: Maksimum 1 Hane ile calisiyor bu sorun cozulecek + Dot Button Logic eksik
+// TODO: Dot Button Logic eksik
 
 import UIKit
 
 var resultNumber: Double = 0
 var resultString = ""
+//var dotString = "."
 
 var additionResult: Double = 0
 var minusResult: Double = 0
@@ -20,12 +21,12 @@ var divideResult: Double = 1
 
 var firstClickInDivide = 0
 
-var additionButtonTapped : Bool = false
-var minusButtonTapped: Bool = false
-var percentButtonTapped: Bool = false
-var multiplyButtonTapped: Bool = false
-var divideButtonTapped: Bool = false
-
+var additionButtonTapped = false
+var minusButtonTapped = false
+var percentButtonTapped = false
+var multiplyButtonTapped = false
+var divideButtonTapped = false
+var dotButtonTapped = false
 
 enum Operators {
     case Addition
@@ -33,10 +34,10 @@ enum Operators {
     case Percent
     case Multiply
     case Divide
+    case Dot
 }
 
 func getOperators(for calculateType: Operators) {
-    
     
     switch calculateType {
         
@@ -45,11 +46,11 @@ func getOperators(for calculateType: Operators) {
         print("Addition icindeki  result:", additionResult)
         
     case .Minus:
-    
+        
         minusResult = -minusResult - resultNumber
         
         print(resultNumber, ".Minus icindeki result number")
-
+        
         
         print("Minus icindeki minus result:", minusResult)
         
@@ -67,301 +68,328 @@ func getOperators(for calculateType: Operators) {
         
     case .Divide:
         
-        // TODO: Divide Logic Hatali
-        
-        print(divideResult, ": divide result")
-     
         if firstClickInDivide == 0 {
             divideResult = resultNumber
         } else {
             divideResult /= resultNumber
         }
-        // esittir divideResilt = resultNumber
         
         print("Divide icindeki divide result: ", divideResult)
         print("Divide icindeki result number: ", resultNumber)
         
-    }
-}
-
-class ViewController: UIViewController {
-    
-    @IBOutlet var resultLabel: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    func equalType() {
-        
-        if additionButtonTapped == true {
-            additionButton(UIButton())
-        }
-        
-        if minusButtonTapped == true {
-            minusButton(UIButton())
-        }
-        
-        if percentButtonTapped == true {
-            percentButton(UIButton())
-        }
-        
-        if multiplyButtonTapped == true {
-            multiplyButton(UIButton())
-        }
-        
-        if divideButtonTapped == true {
-            divideButton(UIButton())
-        }
-    }
-    
-    @IBAction func equalButton(_ sender: UIButton) {
-        
-        equalType()
-        
-        resultNumber = Double(resultString) ?? 0
-        
-        if additionButtonTapped == true {
-            resultLabel.text = "\(additionResult)"
-            
-            print("equal buttondaki aditionresult number:", additionResult)
-        }
-        
-        if minusButtonTapped == true {
-            resultLabel.text = "\(minusResult)"
-            
-            print("equal buttondaki minus result number:", minusResult)
-        }
-        
-        if multiplyButtonTapped == true {
-            resultLabel.text = "\(multiplyResult)"
-            
-            print("equal buttondaki multiply result number:", multiplyResult)
-        }
-        
-        if percentButtonTapped == true {
-            resultLabel.text = "\(percentResult)"
-            
-            print("equal buttondaki percent result number:", percentResult)
-        }
-        
-        if divideButtonTapped == true {
-            resultLabel.text = "\(divideResult)"
-            
-            print("equal buttondaki divide result number:", divideResult)
-        }
-    }
-    
-    @IBAction func ceButton(_ sender: UIButton) {
-        
-        resultLabel.text = "0"
-        resultNumber = 0.0
-        
-        additionResult = 0.0
-        minusResult = 0.0
-        percentResult = 1.0
-        multiplyResult = 1.0
-        divideResult = 1.0
-        
-        firstClickInDivide = 0
-        
-        additionButtonTapped = false
-        minusButtonTapped = false
-        percentButtonTapped = false
-        multiplyButtonTapped = false
-        divideButtonTapped = false
-        
-    }
-    
-    @IBAction func percentButton(_ sender: UIButton) {
-        
-        percentButtonTapped = true
-        
-        getOperators(for: .Percent)
-        
-        resultNumber = 0.0
-        resultLabel.text = ""
-        
-        print("percent icindeki result number:", resultNumber)
-        
-    }
-    
-    @IBAction func additionButton(_ sender: UIButton) {
-        
-        additionButtonTapped = true
-        
-        getOperators(for: .Addition)
-        
-        
-        resultNumber = 0.0
-        resultLabel.text = "0"
-
-    }
-    
-    @IBAction func multiplyButton(_ sender: UIButton) {
-        
-        multiplyButtonTapped = true
-        
-        getOperators(for: .Multiply)
-        
-        resultNumber = 0.0
-        resultLabel.text = "0"
-    }
-    
-    @IBAction func divideButton(_ sender: UIButton) {
-        
-        divideButtonTapped = true
-        
-        
-        getOperators(for: .Divide)
-        
-        resultNumber = 0.0
-        resultLabel.text = ""
-        firstClickInDivide += 1
-    }
-    
-    @IBAction func minusButton(_ sender: UIButton) {
-        
-        getOperators(for: .Minus)
-        
-        minusButtonTapped = true
-        
-        resultNumber = 0.0
-        resultLabel.text = "0"
-        
-    }
-    
-    
-    
-    @IBAction func zeroButton(_ sender: UIButton) {
-        
-        
-        if resultNumber  == 0.0 {
-            resultString = "0"
-        } else {
-            resultString = "\(Int(resultNumber))" + "0"
-        }
-        
-        resultLabel.text = resultString
-        resultNumber = Double(resultString) ?? 0
-        
-    }
-    
-    @IBAction func oneButton(_ sender: UIButton) {
-        
-        if resultNumber  == 0.0 {
-            resultString = "1"
-        } else {
-            resultString = "\(Int(resultNumber))" + "1"
-        }
-        
-        resultLabel.text = resultString
-        resultNumber = Double(resultString) ?? 0
-        
-    }
-    
-    @IBAction func twoButton(_ sender: UIButton) {
-        
-        if resultNumber  == 0.0 {
-            resultString = "2"
-        } else {
-            resultString = "\(Int(resultNumber))" + "2"
-        }
-        
-        resultLabel.text = resultString
-        resultNumber = Double(resultString) ?? 0
-        
-    }
-    
-    @IBAction func threeButton(_ sender: UIButton) {
-        
-        if resultNumber  == 0.0 {
-            resultString = "3"
-        } else {
-            resultString = "\(Int(resultNumber))" + "3"
-        }
-        
-        resultLabel.text = resultString
-        resultNumber = Double(resultString) ?? 0
-
-    }
-    
-    @IBAction func fourButton(_ sender: UIButton) {
-        
-        if resultNumber  == 0.0 {
-            resultString = "4"
-        } else {
-            resultString = "\(Int(resultNumber))" + "4"
-        }
-        
-        resultLabel.text = resultString
-        resultNumber = Double(resultString) ?? 0
-    
-    }
-    
-    @IBAction func fiveButton(_ sender: UIButton) {
-        
-        if resultNumber  == 0.0 {
-            resultString = "5"
-        } else {
-            resultString = "\(Int(resultNumber))" + "5"
-        }
-        
-        resultLabel.text = resultString
-        resultNumber = Double(resultString) ?? 0
-        
-    }
-    
-    @IBAction func sixButton(_ sender: UIButton) {
-        
-        if resultNumber  == 0.0 {
-            resultString = "6"
-        } else {
-            resultString = "\(Int(resultNumber))" + "6"
-        }
-        
-        resultLabel.text = resultString
-        resultNumber = Double(resultString) ?? 0
-        
-    }
-    
-    @IBAction func sevenButton(_ sender: UIButton) {
-        
-        if resultNumber  == 0.0 {
-            resultString = "7"
-        } else {
-            resultString = "\(Int(resultNumber))" + "7"
-        }
-        
-        resultLabel.text = resultString
-        resultNumber = Double(resultString) ?? 0
-        
-    }
-    
-    @IBAction func eightButton(_ sender: UIButton) {
-        
-        if resultNumber  == 0.0 {
-            resultString = "8"
-        } else {
-            resultString = "\(Int(resultNumber))" + "8"
-        }
-        
-        resultLabel.text = resultString
-        resultNumber = Double(resultString) ?? 0
-        
-    }
-    
-    @IBAction func nineButton(_ sender: UIButton) {
-        
-        if resultNumber  == 0.0 {
-            resultString = "9"
-        } else {
-            resultString = "\(Int(resultNumber))" + "9"
-        }
-        
-        resultLabel.text = resultString
-        resultNumber = Double(resultString) ?? 0
+    case .Dot: break
         
     }
 }
-
+    
+    class ViewController: UIViewController {
+        
+        @IBOutlet var resultLabel: UILabel!
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+        }
+        
+        func equalType() {
+            
+            if additionButtonTapped == true {
+                additionButton(UIButton())
+            }
+            
+            if minusButtonTapped == true {
+                minusButton(UIButton())
+            }
+            
+            if percentButtonTapped == true {
+                percentButton(UIButton())
+            }
+            
+            if multiplyButtonTapped == true {
+                multiplyButton(UIButton())
+            }
+            
+            if divideButtonTapped == true {
+                divideButton(UIButton())
+            }
+            
+            if dotButtonTapped == true {
+                dotButton(UIButton())
+            }
+        }
+        
+        @IBAction func equalButton(_ sender: UIButton) {
+            
+            equalType()
+            
+            resultNumber = Double(resultString) ?? 0
+            
+            if additionButtonTapped == true {
+                resultLabel.text = "\(additionResult)"
+                
+                print("equal buttondaki aditionresult number:", additionResult)
+            }
+            
+            if minusButtonTapped == true {
+                resultLabel.text = "\(minusResult)"
+                
+                print("equal buttondaki minus result number:", minusResult)
+            }
+            
+            if multiplyButtonTapped == true {
+                resultLabel.text = "\(multiplyResult)"
+                
+                print("equal buttondaki multiply result number:", multiplyResult)
+            }
+            
+            if percentButtonTapped == true {
+                resultLabel.text = "\(percentResult)"
+                
+                print("equal buttondaki percent result number:", percentResult)
+            }
+            
+            if divideButtonTapped == true {
+                resultLabel.text = "\(divideResult)"
+                
+                print("equal buttondaki divide result number:", divideResult)
+            }
+        }
+        
+        @IBAction func ceButton(_ sender: UIButton) {
+            
+            resultLabel.text = "0"
+            resultNumber = 0.0
+            
+            additionResult = 0.0
+            minusResult = 0.0
+            percentResult = 1.0
+            multiplyResult = 1.0
+            divideResult = 1.0
+            
+            firstClickInDivide = 0
+            
+            additionButtonTapped = false
+            minusButtonTapped = false
+            percentButtonTapped = false
+            multiplyButtonTapped = false
+            divideButtonTapped = false
+            
+        }
+        
+        @IBAction func percentButton(_ sender: UIButton) {
+            
+            percentButtonTapped = true
+            
+            getOperators(for: .Percent)
+            
+            resultNumber = 0.0
+            resultLabel.text = ""
+            
+            print("percent icindeki result number:", resultNumber)
+            
+        }
+        
+        @IBAction func additionButton(_ sender: UIButton) {
+            
+            additionButtonTapped = true
+            
+            getOperators(for: .Addition)
+            
+            
+            resultNumber = 0.0
+            resultLabel.text = "0"
+            
+        }
+        
+        @IBAction func multiplyButton(_ sender: UIButton) {
+            
+            multiplyButtonTapped = true
+            
+            getOperators(for: .Multiply)
+            
+            resultNumber = 0.0
+            resultLabel.text = "0"
+        }
+        
+        @IBAction func divideButton(_ sender: UIButton) {
+            
+            divideButtonTapped = true
+            
+            
+            getOperators(for: .Divide)
+            
+            resultNumber = 0.0
+            resultLabel.text = ""
+            firstClickInDivide += 1
+        }
+        
+        @IBAction func minusButton(_ sender: UIButton) {
+            
+            getOperators(for: .Minus)
+            
+            minusButtonTapped = true
+            
+            resultNumber = 0.0
+            resultLabel.text = "0"
+            
+        }
+        
+        @IBAction func dotButton(_ sender: UIButton) {
+            
+            // TODO: Logic hatali
+            
+            dotButtonTapped = true
+            
+            getOperators(for: .Dot)
+                
+            if (resultNumber == 0.0) {
+                    resultString = "0."
+                resultLabel.text = resultString
+                } else {
+                    if resultLabel.text?.range(of: ".") == nil {
+                        
+                        resultString = resultString + "."
+                        resultLabel.text! = resultString
+                    }
+                }
+                resultLabel.text = resultString
+                
+                
+                print("in dot resultString",resultString)
+                print("in dot resultLab",resultLabel)
+                print("in dot resultNum",resultNumber)
+            
+        }
+        
+        
+        @IBAction func zeroButton(_ sender: UIButton) {
+            
+            
+            if resultNumber  == 0.0 {
+                resultString = "0"
+            } else {
+                resultString = "\(Int(resultNumber))" + "0"
+            }
+            
+            resultLabel.text = resultString
+            resultNumber = Double(resultString) ?? 0
+            
+        }
+        
+        @IBAction func oneButton(_ sender: UIButton) {
+            
+            if resultNumber  == 0.0 {
+                resultString = "1"
+            } else {
+                resultString = "\(Int(resultNumber))" + "1"
+            }
+            
+            resultLabel.text = resultString
+            resultNumber = Double(resultString) ?? 0
+            
+        }
+        
+        @IBAction func twoButton(_ sender: UIButton) {
+            
+            if resultNumber  == 0.0 {
+                resultString = "2"
+            } else {
+                resultString = "\(Int(resultNumber))" + "2"
+            }
+            
+            resultLabel.text = resultString
+            resultNumber = Double(resultString) ?? 0
+            
+        }
+        
+        @IBAction func threeButton(_ sender: UIButton) {
+            
+            if resultNumber  == 0.0 {
+                resultString = "3"
+            } else {
+                resultString = "\(Int(resultNumber))" + "3"
+            }
+            
+            resultLabel.text = resultString
+            resultNumber = Double(resultString) ?? 0
+            
+        }
+        
+        @IBAction func fourButton(_ sender: UIButton) {
+            
+            if resultNumber  == 0.0 {
+                resultString = "4"
+            } else {
+                resultString = "\(Int(resultNumber))" + "4"
+            }
+            
+            resultLabel.text = resultString
+            resultNumber = Double(resultString) ?? 0
+            
+        }
+        
+        @IBAction func fiveButton(_ sender: UIButton) {
+            
+            if resultNumber  == 0.0 {
+                resultString = "5"
+            } else {
+                resultString = "\(Int(resultNumber))" + "5"
+            }
+            
+            resultLabel.text = resultString
+            resultNumber = Double(resultString) ?? 0
+            
+        }
+        
+        @IBAction func sixButton(_ sender: UIButton) {
+            
+            if resultNumber  == 0.0 {
+                resultString = "6"
+            } else {
+                resultString = "\(Int(resultNumber))" + "6"
+            }
+            
+            resultLabel.text = resultString
+            resultNumber = Double(resultString) ?? 0
+            
+        }
+        
+        @IBAction func sevenButton(_ sender: UIButton) {
+            
+            if resultNumber  == 0.0 {
+                resultString = "7"
+            } else {
+                resultString = "\(Int(resultNumber))" + "7"
+            }
+            
+            resultLabel.text = resultString
+            resultNumber = Double(resultString) ?? 0
+            
+        }
+        
+        @IBAction func eightButton(_ sender: UIButton) {
+            
+            if resultNumber  == 0.0 {
+                resultString = "8"
+            } else {
+                resultString = "\(Int(resultNumber))" + "8"
+            }
+            
+            resultLabel.text = resultString
+            resultNumber = Double(resultString) ?? 0
+            
+        }
+        
+        @IBAction func nineButton(_ sender: UIButton) {
+            
+            if resultNumber  == 0.0 {
+                resultString = "9"
+            } else {
+                resultString = "\(Int(resultNumber))" + "9"
+            }
+            
+            resultLabel.text = resultString
+            resultNumber = Double(resultString) ?? 0
+            
+        }
+    }
+    
